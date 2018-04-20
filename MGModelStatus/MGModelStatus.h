e6ef6ed6e0f157261de5753c0aca46ce1a7ecd6d
Mohit Goyal <mohit196thapar@gmail.com>
2018-04-20 11:03:20 +0530

    initial commit

---
+++ MGModelStatus/MGModelStatus.h
@@ -0,0 +1,19 @@
+//
+//  MGModelStatus.h
+//  MGModelStatus
+//
+//  Created by Mohit Goyal on 19/04/18.
+//  Copyright © 2018 Mohit Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGModelStatus.
+FOUNDATION_EXPORT double MGModelStatusVersionNumber;
+
+//! Project version string for MGModelStatus.
+FOUNDATION_EXPORT const unsigned char MGModelStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGModelStatus/PublicHeader.h>
+
+
